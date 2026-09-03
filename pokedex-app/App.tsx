0ad062040e28927c6c833017da62e5b942105d84
Levi-Work <leviwork@Levis-MacBook-Pro.local>
2026-09-03 12:30:04 +0200

    feature: Pokemon list and ID

--- pokedex-app/App.tsx
+++ pokedex-app/App.tsx
@@ -1,5 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
-  return <HomeScreen />;
+	return (
+		<SafeAreaProvider>
+			<SafeAreaView style={styles.safeArea}>
+				<HomeScreen />
+			</SafeAreaView>
+		</SafeAreaProvider>
+	);
 }
+
+const styles = StyleSheet.create({
+	safeArea: {
+		flex: 1,
+		backgroundColor: 'black',
+	},
+});
